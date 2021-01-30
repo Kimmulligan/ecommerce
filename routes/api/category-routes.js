81856849b52cba436f8121c2d0bbee8cb7e913a0
Kim Mulligan <kimmariemulligan@gmail.com>
2021-01-29 19:49:25 -0800

    getting 'wrong route' error

---
+++ routes/api/category-routes.js
@@ -0,0 +1,67 @@
+const router = require('express').Router();
+const { Category, Product } = require('../../models');
+
+// The `/api/categories` endpoint
+
+router.get('/', (req, res) => {
+  // find all categories
+  // be sure to include its associated Products
+  Category.findAll({
+    // Here we add an "include" property to our options in our findAll query
+    // We set the value to an array of the models we want to include in a left outer join
+    // In this case, just `db.Post`
+    include: [Product]
+  }).then(category => {
+    res.json(category);
+  });
+});
+
+router.get('/:id', (req, res) => {
+  // find one category by its `id` value
+  // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id
+    },
+    // Here we add an "include" property to our options in our findOne query
+    // We set the value to an array of the models we want to include in a left outer join
+    // In this case, just `db.Post`
+    include: [Product]
+  }).then(category => {
+    res.json(category);
+  });
+});
+
+router.post('/', (req, res) => {
+  // create a new category
+  Category.create(req.body)
+  .then(category =>{
+    res.json(category);
+  })
+});
+
+router.put('/:id', (req, res) => {
+  // update a category by its `id` value
+  Category.update(req.body,
+    {
+      where: {
+        id: req.params.id
+      }
+    }
+  ).then(Category => {
+    res.json(Category);
+  });
+});
+
+router.delete('/:id', (req, res) => {
+  // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  }).then(Category => {
+    res.json(Category);
+  });
+});
+
+module.exports = router;
